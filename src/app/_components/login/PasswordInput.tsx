0e8c3013757a10e1129b18d60b01170e3007696b
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-07-08 00:38:09 +0900

    style: 비밀번호 입력 컴포넌트 생성

---
+++ src/app/_components/login/PasswordInput.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React, { useState, ChangeEvent } from "react";
+
+function PasswordInput() {
+  const [password, setPassword] = useState<string>("");
+
+  const handlePassword = (e: ChangeEvent<HTMLInputElement>) =>
+    setPassword(e.target.value);
+  return (
+    <div className="ml-9 mt-12 w-full">
+      <div className="text-[#A5A5A5] font-light text-sm">비밀번호</div>
+      <div className="flex items-center">
+        <input
+          type="password"
+          className="w-[415px] p-2 border-b-2 border-[#A5A5A5] border-x-0 border-t-0 outline-none focus-within:border-main transition duration-300 placeholder:font-light placeholder:text-sm"
+          placeholder="영문·숫자·특수문자 포함 8자 이상"
+          value={password}
+          onChange={handlePassword}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default PasswordInput;
