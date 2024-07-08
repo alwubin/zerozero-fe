ee71e396e2d15fb0334c69948d5da1ea3272c3b1
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-07-08 16:47:03 +0900

    feat: 회원가입 페이지 이동 로직 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,19 @@ export default function Home() {
   const handleLogin = () => {
     router.push("/login");
   };
+
+  const handleSignup = () => {
+    router.push("/signup");
+  };
+
   return (
     <div className="flex min-h-screen flex-col bg-white">
       <AnimatedLogo />
       <div className="w-full flex flex-col items-center">
-        <button className="w-[90%] h-12 bg-main text-white rounded-lg font-bold">
+        <button
+          className="w-[90%] h-12 bg-main text-white rounded-lg font-bold"
+          onClick={handleSignup}
+        >
           시작하기
         </button>
         <div className="flex justify-center mt-14 mb-14 text-[#A5A5A5] font-bold">
