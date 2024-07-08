a65aa51409bc316175794676f99bc33ba69ffc3f
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-07-08 18:30:48 +0900

    style: 회원가입 페이지 UI 구현

--- src/app/(route)/signup/page.tsx
+++ src/app/(route)/signup/page.tsx
@@ -1,10 +1,20 @@
-import { useRouter } from "next/navigation";
 import BackButton from "@/app/_components/common/BackButton";
+import HeaderMessage from "@/app/_components/common/HeaderMessage";
+import NicknameInput from "@/app/_components/login/NicknameInput";
+import EmailInput from "@/app/_components/login/EmailInput";
+import PasswordInput from "@/app/_components/login/PasswordInput";
+import ConfirmButton from "@/app/_components/common/ConfirmButton";
 
 export default function Signup() {
   return (
     <div className="flex min-h-screen flex-col bg-white">
       <BackButton />
+      <HeaderMessage message="닉네임을 입력해주세요" />
+      <NicknameInput />
+      <EmailInput className="ml-9 mt-12 w-full" />
+      <PasswordInput label="비밀번호" />
+      <PasswordInput label="비밀번호 확인" />
+      <ConfirmButton />
     </div>
   );
 }
