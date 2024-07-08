c728865578706e07977496c1bedc4e4ee68d8058
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-07-08 16:55:53 +0900

    feat: 뒤로가기 버튼 컴포넌트 생성 및 페이지에 적용

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -1,29 +1,11 @@
-"use client";
-import Image from "next/image";
 import EmailInput from "../../_components/login/EmailInput";
 import PasswordInput from "../../_components/login/PasswordInput";
-import { useRouter } from "next/navigation";
+import BackButton from "@/app/_components/common/BackButton";
 
 export default function Login() {
-  const router = useRouter();
-
-  const handleBackButton = () => {
-    router.push("/");
-  };
-
   return (
     <div className="flex min-h-screen flex-col bg-white">
-      <div
-        className="justify-start ml-9 mt-9 cursor-pointer"
-        onClick={handleBackButton}
-      >
-        <Image
-          src="/images/back-button.png"
-          alt="back-button"
-          width={24}
-          height={24}
-        />
-      </div>
+      <BackButton />
       <div className="font-bold text-2xl ml-9 mt-14">
         이메일과 비밀번호로 로그인해주세요
       </div>
---
+++ src/app/(route)/signup/page.tsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/navigation";
+import BackButton from "@/app/_components/common/BackButton";
+
+export default function Signup() {
+  return (
+    <div className="flex min-h-screen flex-col bg-white">
+      <BackButton />
+    </div>
+  );
+}
