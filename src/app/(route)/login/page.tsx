1cd9748992d4ed5c0f072bf9a7656c4348082b26
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-07-09 19:41:56 +0900

    feat: 컴포넌트 재마운트 시 상태 초기화되는 로직 추가

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -1,16 +1,26 @@
+"use client";
+import { useLoginStore } from "@/app/store/loginStore";
 import EmailInput from "../../_components/login/EmailInput";
 import PasswordInput from "../../_components/login/PasswordInput";
 import BackButton from "@/app/_components/common/BackButton";
 import ConfrimButton from "@/app/_components/common/ConfirmButton";
 import HeaderMessage from "@/app/_components/common/HeaderMessage";
+import { useEffect } from "react";
 
 export default function Login() {
+  const { email, password, setEmail, setPassword, resetState } =
+    useLoginStore();
+
+  useEffect(() => {
+    resetState();
+  }, [resetState]);
+
   return (
     <div className="flex min-h-screen flex-col bg-white">
       <BackButton />
       <HeaderMessage message="이메일과 비밀번호로 로그인해주세요" />
-      <EmailInput />
-      <PasswordInput />
+      <EmailInput email={email} setEmail={setEmail} />
+      <PasswordInput label="비밀번호" value={password} onChange={setPassword} />
       <ConfrimButton />
     </div>
   );
--- src/app/(route)/signup/page.tsx
+++ src/app/(route)/signup/page.tsx
@@ -7,8 +7,18 @@ import NicknameInput from "@/app/_components/login/NicknameInput";
 import EmailInput from "@/app/_components/login/EmailInput";
 import PasswordInput from "@/app/_components/login/PasswordInput";
 import ConfirmButton from "@/app/_components/common/ConfirmButton";
+import { useEffect } from "react";
 
 export default function Signup() {
+  const {
+    email,
+    password,
+    confirmPassword,
+    setEmail,
+    setPassword,
+    setConfirmPassword,
+    resetState,
+  } = useSignupStore();
   const step = useSignupStore((state) => state.step);
 
   const getHeaderMessage = () => {
@@ -23,6 +33,10 @@ export default function Signup() {
         return "";
     }
   };
+
+  useEffect(() => {
+    resetState();
+  }, [resetState]);
 
   return (
     <div className="flex min-h-screen flex-col bg-white">
@@ -43,7 +57,11 @@ export default function Signup() {
           animate={{ opacity: 1 }}
           transition={{ delay: 0.3, duration: 0.5 }}
         >
-          <EmailInput className="ml-9 mt-12 w-full" />
+          <EmailInput
+            className="ml-9 mt-12 w-full"
+            email={email}
+            setEmail={setEmail}
+          />
         </motion.div>
       )}
       {step >= 3 && (
@@ -52,8 +70,17 @@ export default function Signup() {
           animate={{ opacity: 1 }}
           transition={{ delay: 0.3, duration: 0.5 }}
         >
-          <PasswordInput label="비밀번호" isConfirm={false} />
-          <PasswordInput label="비밀번호 확인" isConfirm={true} />
+          <PasswordInput
+            label="비밀번호"
+            value={password}
+            onChange={setPassword}
+          />
+          <PasswordInput
+            label="비밀번호 확인"
+            value={confirmPassword}
+            onChange={setConfirmPassword}
+            isConfirm={true}
+          />
         </motion.div>
       )}
       <ConfirmButton />
