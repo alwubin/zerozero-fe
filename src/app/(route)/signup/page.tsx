147e3fa7a6340a4339930782e65ad2a0c8a7e062
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-07-09 21:54:58 +0900

    feat: 각 컴포넌트마다 정규식 오류 애니메이션 적용

--- src/app/(route)/signup/page.tsx
+++ src/app/(route)/signup/page.tsx
@@ -1,25 +1,29 @@
 "use client";
 import { useSignupStore } from "@/app/store/signupStore";
-import { motion } from "framer-motion";
 import BackButton from "@/app/_components/common/BackButton";
 import HeaderMessage from "@/app/_components/common/HeaderMessage";
 import NicknameInput from "@/app/_components/login/NicknameInput";
 import EmailInput from "@/app/_components/login/EmailInput";
 import PasswordInput from "@/app/_components/login/PasswordInput";
 import ConfirmButton from "@/app/_components/common/ConfirmButton";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Signup() {
   const {
+    step,
     email,
     password,
     confirmPassword,
+    setStep,
     setEmail,
     setPassword,
     setConfirmPassword,
     resetState,
   } = useSignupStore();
-  const step = useSignupStore((state) => state.step);
+
+  const [nicknameShake, setNicknameShake] = useState(false);
+  const [emailShake, setEmailShake] = useState(false);
+  const [passwordShake, setPasswordShake] = useState(false);
 
   const getHeaderMessage = () => {
     switch (step) {
@@ -34,6 +38,37 @@ export default function Signup() {
     }
   };
 
+  const handleConfirm = () => {
+    if (step === 1) {
+      const regex = /^[ㄱ-ㅎ가-힣a-zA-Z0-9]{2,10}$/;
+      if (!regex.test(useSignupStore.getState().nickname)) {
+        setNicknameShake(true);
+        setTimeout(() => setNicknameShake(false), 200);
+      } else {
+        setStep(step + 1);
+      }
+    } else if (step === 2) {
+      const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+      if (!regex.test(useSignupStore.getState().email)) {
+        setEmailShake(true);
+        setTimeout(() => setEmailShake(false), 200);
+      } else {
+        setStep(step + 1);
+      }
+    } else {
+      const regex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,16}$/;
+      if (
+        !regex.test(useSignupStore.getState().password) &&
+        !regex.test(useSignupStore.getState().confirmPassword)
+      ) {
+        setPasswordShake(true);
+        setTimeout(() => setPasswordShake(false), 200);
+      } else {
+        alert("가입되었습니다.");
+      }
+    }
+  };
+
   useEffect(() => {
     resetState();
   }, [resetState]);
@@ -42,48 +77,32 @@ export default function Signup() {
     <div className="flex min-h-screen flex-col bg-white">
       <BackButton />
       <HeaderMessage message={getHeaderMessage()} />
-      {step >= 1 && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.3, duration: 0.5 }}
-        >
-          <NicknameInput />
-        </motion.div>
-      )}
+      {step >= 1 && <NicknameInput onShake={nicknameShake} />}
       {step >= 2 && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.3, duration: 0.5 }}
-        >
-          <EmailInput
-            className="ml-9 mt-12 w-full"
-            email={email}
-            setEmail={setEmail}
-          />
-        </motion.div>
+        <EmailInput
+          className="ml-9 mt-12 w-full"
+          setEmail={setEmail}
+          onShake={emailShake}
+        />
       )}
       {step >= 3 && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.3, duration: 0.5 }}
-        >
+        <>
           <PasswordInput
             label="비밀번호"
             value={password}
+            onShake={passwordShake}
             onChange={setPassword}
           />
           <PasswordInput
             label="비밀번호 확인"
             value={confirmPassword}
+            onShake={passwordShake}
             onChange={setConfirmPassword}
             isConfirm={true}
           />
-        </motion.div>
+        </>
       )}
-      <ConfirmButton />
+      <ConfirmButton handleConfirm={handleConfirm} />
     </div>
   );
 }
