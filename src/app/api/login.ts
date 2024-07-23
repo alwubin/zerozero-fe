ef66fdd70a9a494a84f5d9eda455aa91e6164d81
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-07-23 23:37:23 +0900

    feat: 로그인 및 로그아웃 기능 구현

---
+++ src/app/api/login.ts
@@ -0,0 +1,29 @@
+import { axiosInstance } from "./interceptor"
+
+interface LoginProps {
+    email: string;
+    password: string;
+  }
+
+export const postLogin = async (values: LoginProps) => {
+    try {
+        const response = await axiosInstance.post('/login', values);
+
+        if (response.status === 200) {
+            const tokens = response.data.token;
+            localStorage.setItem('accessToken', tokens.accessToken.token);
+            localStorage.setItem('refreshToken', tokens.refreshToken.token);
+            return true;
+        } else {
+            return false;
+        }
+    } catch (error) {
+        console.log('로그인 실패: ', error);
+    }
+};
+
+export const logout = () => {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('pubId');
+}
