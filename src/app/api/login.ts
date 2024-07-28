0c5bbcc50d3579a8ba9ed24333134220a968b095
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-07-28 23:48:57 +0900

    fix: 로그인 시 토큰 저장 방식 수정

--- src/app/api/login.ts
+++ src/app/api/login.ts
@@ -1,40 +1,51 @@
-import { axiosInstance } from "./interceptor"
-import { destroyCookie } from 'nookies';
+import { axiosInstance } from "./interceptor";
+import { setCookie, destroyCookie } from 'nookies';
+import { GetServerSidePropsContext } from 'next';
 
 interface LoginProps {
-    email: string;
-    password: string;
-  }
-  
-  interface Token {
-    token: string;
-  }
-  
-  interface LoginResponse {
-    token: {
-      accessToken: Token;
-      refreshToken: Token;
-    };
-  }
-  
-  export const postLogin = async (values: LoginProps): Promise<boolean> => {
-    try {
-      const { data } = await axiosInstance.post<LoginResponse>('/login', values);
-      
-      const { accessToken, refreshToken } = data.token;
-      localStorage.setItem('accessToken', accessToken.token);
-      localStorage.setItem('refreshToken', refreshToken.token);
-      
-      return true;
-    } catch (error) {
-      console.error('로그인 실패:', error);
-      throw new Error('로그인에 실패했습니다.');
-    }
-  };
+  email: string;
+  password: string;
+}
 
-  export const logout = () => {
-    destroyCookie(null, 'accessToken');
-    destroyCookie(null, 'refreshToken');
-    window.location.href = '/';
+interface Token {
+  token: string;
+}
+
+interface LoginResponse {
+  token: {
+    accessToken: Token;
+    refreshToken: Token;
   };
-  
+}
+
+export const postLogin = async (values: LoginProps, ctx?: GetServerSidePropsContext): Promise<boolean> => {
+  try {
+    const { data } = await axiosInstance.post<LoginResponse>('/login', values);
+
+    const { accessToken, refreshToken } = data.token;
+
+    // nookies를 사용하여 쿠키에 토큰을 저장합니다.
+    setCookie(ctx, 'accessToken', accessToken.token, {
+      maxAge: 30 * 24 * 60 * 60, // 30일
+      path: '/',
+    });
+
+    setCookie(ctx, 'refreshToken', refreshToken.token, {
+      maxAge: 30 * 24 * 60 * 60, // 30일
+      path: '/',
+    });
+
+    return true;
+  } catch (error) {
+    console.error('로그인 실패:', error);
+    throw new Error('로그인에 실패했습니다.');
+  }
+};
+
+export const logout = (ctx?: GetServerSidePropsContext) => {
+  destroyCookie(ctx, 'accessToken');
+  destroyCookie(ctx, 'refreshToken');
+  if (typeof window !== 'undefined') {
+    window.location.href = '/';
+  }
+};
