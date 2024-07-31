d33056a889d3bbc40fad53f0dc08fffc2f6e213d
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-08-01 01:56:09 +0900

    chore: svg 파일을 위한 모듈 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack(config) {
+        // SVG 파일을 처리하기 위한 rule 추가
+        config.module.rules.push({
+          test: /\.svg$/i,
+          use: [
+            {
+              loader: '@svgr/webpack',
+            },
+          ],
+        });
+    
+        return config;
+      },
+};
 
 export default nextConfig;
