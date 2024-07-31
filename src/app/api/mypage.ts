edfe41241ae196b79828e918d65a39df96dcbcf8
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-07-31 23:25:19 +0900

    feat: 마이페이지 프로필 업로드 api 구현

--- src/app/api/mypage.ts
+++ src/app/api/mypage.ts
@@ -1,4 +1,5 @@
 import { axiosInstance } from "./interceptor"
+import { AxiosResponse } from 'axios';
 
 interface UserProfileResponse {
     success: boolean;
@@ -11,6 +12,10 @@ interface UserProfileResponse {
         storeReportCount: number; // 기본값 0
     };
 }
+
+interface UploadImageResponse {
+    imageUrl: string;
+  }
 
 export const getUserProfile = async (): Promise<UserProfileResponse['userProfile'] | null> => {
     try {
@@ -27,3 +32,22 @@ export const getUserProfile = async (): Promise<UserProfileResponse['userProfile
         return null;
     }
 }
+
+  
+export const uploadImage = async (imageFile: File): Promise<UploadImageResponse> => {
+    try {
+      const formData = new FormData();
+      formData.append('imageFile', imageFile);
+  
+      const response: AxiosResponse<UploadImageResponse> = await axiosInstance.post('/user/image', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+  
+      return response.data;
+    } catch (error) {
+      console.error('이미지 업로드 중 오류 발생:', error);
+      throw error;
+    }
+};
