3e956823c1480763a6a32c43141d42bbaf97d42b
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-08-02 01:56:42 +0900

    chore: 이미지 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-        // SVG 파일을 처리하기 위한 rule 추가
-        config.module.rules.push({
-          test: /\.svg$/i,
-          use: [
-            {
-              loader: '@svgr/webpack',
-            },
-          ],
-        });
-    
-        return config;
-      },
+  webpack(config) {
+    // SVG 파일을 처리하기 위한 rule 추가
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [
+        {
+          loader: '@svgr/webpack',
+        },
+      ],
+    });
+
+    return config;
+  },
+  images: {
+    domains: ['s3.ap-northeast-2.amazonaws.com'],
+  },
 };
 
 export default nextConfig;
