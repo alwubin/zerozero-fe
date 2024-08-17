eae8e1ff4312455f71bcc89f768c5e95549b8b40
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-08-17 21:03:50 +0900

    feat: 초기화 상태 관리 추가

--- src/app/store/reportStore.ts
+++ src/app/store/reportStore.ts
@@ -1,11 +1,12 @@
-import { create } from 'zustand'
-import { SearchResult } from '@/app/_components/report/SearchBar'; 
+import { create } from 'zustand';
+import { SearchResult } from '@/app/_components/report/SearchBar';
 
 interface SearchStoreState {
   searchResults: SearchResult[];
   query: string;
   setSearchResults: (results: SearchResult[]) => void;
   setQuery: (query: string) => void;
+  resetSearchStore: () => void;  
 }
 
 interface SelectStoreState {
@@ -19,6 +20,7 @@ interface SelectStoreState {
   setLongitude: (longitude: string) => void;
   setLatitude: (latitude: string) => void;
   setImageFiles: (imageFiles: File[]) => void;
+  resetSelectStore: () => void;  
 }
 
 export const useSearchStore = create<SearchStoreState>((set) => ({
@@ -26,8 +28,8 @@ export const useSearchStore = create<SearchStoreState>((set) => ({
   query: '',
   setSearchResults: (results) => set({ searchResults: results }),
   setQuery: (query) => set({ query }),
+  resetSearchStore: () => set({ searchResults: [], query: '' }),  
 }));
-
 
 export const useSelectStore = create<SelectStoreState>((set) => ({
   placeName: '',
@@ -40,4 +42,12 @@ export const useSelectStore = create<SelectStoreState>((set) => ({
   setLongitude: (longitude) => set({ longitude }),
   setLatitude: (latitude) => set({ latitude }),
   setImageFiles: (imageFiles) => set({ imageFiles }),
+  resetSelectStore: () =>
+    set({
+      placeName: '',
+      address: '',
+      longitude: '',
+      latitude: '',
+      imageFiles: [],
+    }),  
 }));
