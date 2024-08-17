2d41e535c849df9f6f95a32071b5176579e0115f
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-08-18 02:09:29 +0900

    feat: 판매점 상세페이지 조회 로직 구현

--- src/app/(route)/detail/page.tsx
+++ src/app/(route)/detail/page.tsx
@@ -1,19 +1,44 @@
-"use client";
-import { useState } from "react";
-import { DetailHeader } from "@/app/_components/detail/DetailHeader";
-import { StoreInfo } from "@/app/_components/detail/StoreInfo";
-import { RegisteredStoreImage } from "@/app/_components/detail/RegisteredStoreImage";
-import { StoreImage } from "@/app/_components/report/StoreImages";
-import { StoreReview } from "@/app/_components/detail/StoreReview";
-import { ReportButton } from "@/app/_components/report/ReportButton";
+'use client';
+import React, { useEffect } from 'react';
+import { DetailHeader } from '@/app/_components/detail/DetailHeader';
+import { StoreInfo } from '@/app/_components/detail/StoreInfo';
+import { RegisteredStoreImage } from '@/app/_components/detail/RegisteredStoreImage';
+import StoreImage from '@/app/_components/report/StoreImages';
+import { StoreReview } from '@/app/_components/detail/StoreReview';
+import { ReportButton } from '@/app/_components/report/ReportButton';
+import { useSelectedStore } from '@/app/store/detailStore';
+import { useSelectStore } from '@/app/store/reportStore';
+import { getStoreInfo } from '@/app/api/detail';
 
 export default function Detail() {
-  const [isRegistered, setIsRegistered] = useState(false);
+  const { status, id } = useSelectStore();
+  const { setImages, setReviews, setZeroDrinks, filter } = useSelectedStore();
+
+  const fetchStoreInfo = async () => {
+    try {
+      const storeInfo = await getStoreInfo(id, filter);
+      if (storeInfo) {
+        setImages(storeInfo.store.images || []);
+        setReviews(storeInfo.reviews || []);
+        setZeroDrinks(storeInfo.zeroDrinks || [[]]);
+        console.log(storeInfo);
+      }
+    } catch (error) {
+      console.error('판매점 조회 오류', error);
+    }
+  };
+
+  useEffect(() => {
+    if (status && id) {
+      fetchStoreInfo();
+    }
+  }, [status, id, filter]);
+
   return (
     <div className="flex min-h-screen flex-col bg-[#F2F4F6]">
       <DetailHeader />
       <StoreInfo />
-      {isRegistered ? (
+      {status ? (
         <div>
           <RegisteredStoreImage />
           <StoreReview />
