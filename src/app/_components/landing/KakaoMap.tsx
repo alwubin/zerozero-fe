b785cedb6497ac68772136052c48507746ebcc0d
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-10-05 14:09:41 +0900

    feat: 웹소켓 연결 구현

--- src/app/_components/landing/KakaoMap.tsx
+++ src/app/_components/landing/KakaoMap.tsx
@@ -1,5 +1,7 @@
+import { useEffect, useState } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import { StoreProps } from '@/app/(route)/landing/page';
+import { parseCookies } from 'nookies'; // 쿠키에서 토큰을 가져오기 위해 추가
 
 interface KakaoMapProps {
   center: { lat: number; lng: number };
@@ -14,6 +16,55 @@ export function KakaoMap({
   clickedIndex,
   onMarkerClick,
 }: KakaoMapProps) {
+  const [updatedStoreList, setUpdatedStoreList] = useState<StoreProps[] | null>(
+    storeList,
+  );
+
+  useEffect(() => {
+    const cookies = parseCookies();
+    const accessToken = cookies.accessToken;
+
+    const socket = new WebSocket('ws://3.37.245.108:8080/ws/store');
+
+    socket.onopen = () => {
+      console.log('WebSocket 연결됨');
+
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+
+        const message = {
+          longitude: longitude,
+          latitude: latitude,
+          accessToken: accessToken,
+        };
+
+        socket.send(JSON.stringify(message));
+      });
+    };
+
+    socket.onmessage = (event) => {
+      try {
+        const updatedStores: StoreProps[] = JSON.parse(event.data);
+        console.log(updatedStores);
+        setUpdatedStoreList(updatedStores);
+      } catch (err) {
+        console.error('웹소켓 메시지 처리 중 오류:', err);
+      }
+    };
+
+    socket.onerror = (error) => {
+      console.error('WebSocket 오류:', error);
+    };
+
+    socket.onclose = () => {
+      console.log('WebSocket 연결이 종료되었습니다.');
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
   return (
     <Map
       center={center}
