3b121dc4be1a2710c3cbd75bc57e0b4bd4dc5d4c
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-01-19 02:01:57 +0900

    feat: 카카오 로그인 버튼 UI 구현

--- src/app/(route)/(home)/page.tsx
+++ src/app/(route)/(home)/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { KakaoLogo } from '@/app/assets';
 import AnimatedLogo from '../../_components/home/AnimatedLogo';
 import { useRouter } from 'next/navigation';
 
@@ -16,14 +17,23 @@ export default function Home() {
   return (
     <div className="flex w-full min-h-screen flex-col bg-white">
       <AnimatedLogo />
-      <div className="w-full flex flex-col items-center">
+      <div className="w-full flex flex-col items-center space-y-3 mb-10">
         <button
-          className="w-[90%] h-12 bg-main text-white rounded-lg font-bold"
+          className="flex items-center cursor-pointer justify-center rounded-md bg-[#fee500] p-4 space-x-7 w-[90%] "
+          onClick={handleLogin}
+        >
+          <KakaoLogo />
+          <span className="text-black font-semibold text-base">
+            카카오로 3초만에 시작하기
+          </span>
+        </button>
+        <button
+          className="w-[90%] p-4 bg-main text-white rounded-lg cursor-pointer font-semibold"
           onClick={handleSignup}
         >
           시작하기
         </button>
-        <div className="flex justify-center mt-14 mb-14 text-[#A5A5A5] font-bold">
+        <div className="flex justify-center text-[#A5A5A5] font-semibold">
           이미 계정이 있나요?{' '}
           <button
             className="text-main ml-2 font-bold hover:cursor"
