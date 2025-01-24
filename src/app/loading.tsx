116df10803fcb3cb8e437bf5e7a662c98b42b30a
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-01-24 15:22:59 +0900

    style: 로딩 페이지 배경 스타일 추가

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,7 +5,7 @@ import spinner from '@/app/assets/spinner.json';
 
 const Loading = () => {
   return (
-    <div className="space-y-2 flex h-screen flex-col items-center justify-center">
+    <div className="space-y-2 flex h-screen flex-col items-center justify-center bg-white">
       <Lottie loop animationData={spinner} play />
       <div className="pl-[7.5px] text-gray-400 text-base font-semibold">
         로딩 중...
