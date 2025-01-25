8baff946207181fa96a4733ef9616b726e341d01
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-01-25 20:05:46 +0900

    refactor: 컴포넌트 캡슐화를 개선하기 위해 전역 상태 대신 props를 전달하도록 리팩토링

--- src/app/(route)/store/[id]/page.tsx
+++ src/app/(route)/store/[id]/page.tsx
@@ -1,55 +1,81 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 'use client';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DetailHeader } from '@/app/_components/store/DetailHeader';
 import { StoreInfo } from '@/app/_components/store/StoreInfo';
 import { RegisteredStoreImage } from '@/app/_components/store/RegisteredStoreImage';
 import { StoreReview } from '@/app/_components/store/StoreReview';
 import { useSelectedStore } from '@/app/zustand/detailStore';
-import { useSelectStore } from '@/app/zustand/reportStore';
 import { getStoreInfo } from '@/app/api/detail';
+import { useParams } from 'next/navigation';
+import Loading from '@/app/loading';
+
+export interface Store {
+  id: string;
+  kakaoId: string;
+  name: string;
+  category: string;
+  phone: string;
+  address: string;
+  roadAddress: string;
+  longitude: string;
+  latitude: string;
+  status: boolean;
+  images: Image[];
+  placeUrl: string;
+}
+
+export interface Image {
+  url: string;
+}
 
 export default function RegisteredStore() {
-  const { status, id } = useSelectStore();
-  const {
-    setStoreId,
-    setImages,
-    setReviews,
-    setZeroDrinks,
-    setFilter,
-    storeId,
-    filter,
-  } = useSelectedStore();
+  const params = useParams<{ id: string }>();
+  const { filter } = useSelectedStore();
+  const [isLoading, setIsLoading] = useState(true);
+  const [storeInfo, setStoreInfo] = useState<Store>();
+  const [storeId, setStoreId] = useState<string>('');
 
   const fetchStoreInfo = async (value: string) => {
     try {
       const storeInfo = await getStoreInfo(value, filter);
+      console.log(storeInfo.store);
       if (storeInfo) {
-        setStoreId(storeInfo.store.id || '');
-        setImages(storeInfo.store.images || []);
-        setReviews(storeInfo.reviews || []);
-        setZeroDrinks(storeInfo.zeroDrinks || [[]]);
-        console.log(storeInfo);
+        setStoreInfo(storeInfo.store);
+        // setStoreId(storeInfo.store.id || '');
+        // setImages(storeInfo.store.images || []);
+        // setReviews(storeInfo.reviews || []);
+        // setZeroDrinks(storeInfo.zeroDrinks || [[]]);
       }
     } catch (error) {
       console.error('판매점 조회 오류', error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    console.log('Status:', status);
-    console.log('ID:', id);
-    console.log('Store ID:', storeId);
-    console.log('Filter:', filter);
-    fetchStoreInfo(id);
+    if (params?.id) {
+      fetchStoreInfo(params.id);
+      setStoreId(params.id);
+    }
   }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <div className="flex min-h-screen flex-col bg-[#F2F4F6]">
       <DetailHeader />
-      <StoreInfo />
+      <StoreInfo
+        placeName={storeInfo?.name}
+        phone={storeInfo?.phone}
+        address={storeInfo?.address}
+        category={storeInfo?.category}
+      />
       <div>
-        <RegisteredStoreImage />
-        <StoreReview />
+        <RegisteredStoreImage images={storeInfo?.images} />
+        <StoreReview storeId={storeId} />
       </div>
     </div>
   );
