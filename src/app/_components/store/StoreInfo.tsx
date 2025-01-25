c1ec8d35019739adee3d9aac41d7254090e39186
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-01-25 20:07:12 +0900

    refactor: props로 전달된 값이 없을 경우 전역 상태에서 값을 가져오도록 로직 수정

--- src/app/_components/store/StoreInfo.tsx
+++ src/app/_components/store/StoreInfo.tsx
@@ -2,24 +2,42 @@
 import { useSelectStore } from '@/app/zustand/reportStore';
 import { Marker, Phone, Category } from '@/app/assets';
 
-export const StoreInfo = () => {
-  const { placeName, phone, address, category } = useSelectStore();
+interface StoreInfoProps {
+  placeName?: string;
+  phone?: string;
+  address?: string;
+  category?: string;
+}
+
+export const StoreInfo = ({
+  placeName,
+  phone,
+  address,
+  category,
+}: StoreInfoProps) => {
+  const selectStore = useSelectStore();
+
+  const displayPlaceName = placeName || selectStore.placeName;
+  const displayPhone = phone || selectStore.phone;
+  const displayAddress = address || selectStore.address;
+  const displayCategory = category || selectStore.category;
+
   return (
     <div className="w-10/12 bg-white p-6 rounded-2xl space-y-4 mt-8 ml-10">
-      <div className="font-bold text-2xl">{placeName}</div>
+      <div className="font-bold text-2xl">{displayPlaceName}</div>
       <div className="flex flex-row font-extralight text-sm">
         <Marker className="mr-2" />
-        {address}
+        {displayAddress}
       </div>
 
       <div className="flex flex-row font-extralight text-sm">
         <Phone className="mr-2" />
-        {phone ? phone : <p>제공되지 않음</p>}
+        {displayPhone ? displayPhone : <p>제공되지 않음</p>}
       </div>
 
       <div className="flex flex-row font-extralight text-sm">
         <Category className="mr-2" />
-        {category}
+        {displayCategory}
       </div>
     </div>
   );
