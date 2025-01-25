77da9c78c2b54b45656621bbd3fa9e81dab7ce64
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-01-25 21:14:38 +0900

    refactor: Header BackButton 클릭 핸들러 수정

--- src/app/_components/common/BackButton.tsx
+++ src/app/_components/common/BackButton.tsx
@@ -1,16 +1,12 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { BackIcon } from "@/app/assets";
+'use client';
+import { useRouter } from 'next/navigation';
+import { BackIcon } from '@/app/assets';
 
 const BackButton = () => {
   const router = useRouter();
 
   const handleBackButton = () => {
-    if (window.history.length > 1) {
-      router.back();
-    } else {
-      router.push("/");
-    }
+    router.push('/');
   };
 
   return (
