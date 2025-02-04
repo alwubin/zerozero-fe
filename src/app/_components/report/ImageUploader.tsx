eb2ff5b0df821dbd7df1740259ca1f0380c63699
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-02-04 17:55:09 +0900

    fix: prefix 오류 수정

--- src/app/_components/report/ImageUploader.tsx
+++ src/app/_components/report/ImageUploader.tsx
@@ -5,10 +5,7 @@ import { ImagePreview } from '@/app/_components/report/ImagePreview';
 import { ImageUploadButton } from '@/app/_components/report/ImageUploadButton';
 
 export const ImageUploader = () => {
-  const { images, addImage, isFull, isUploading } = useImageUpload(
-    'store-images',
-    3,
-  );
+  const { images, addImage, isFull, isUploading } = useImageUpload('store', 3);
 
   return (
     <div className="bg-white flex flex-col space-y-3 rounded-xl p-4 pb-6 mt-4">
@@ -19,7 +16,7 @@ export const ImageUploader = () => {
         onUpload={addImage}
       />
       {isUploading ? (
-        <div className="text-center">업로드 중...</div>
+        <div className="text-center text-xs text-gray-300">업로드 중...</div>
       ) : (
         images.map((image, index) => (
           <ImagePreview key={index} imgUrl={image} />
