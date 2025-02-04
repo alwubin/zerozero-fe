0e8ff5927484a63f90005a88530f93ee472d8f01
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-02-04 19:23:41 +0900

    refactor: 이미지 업로드 시 가로 정렬 되도록 수정

--- src/app/_components/report/ImageUploader.tsx
+++ src/app/_components/report/ImageUploader.tsx
@@ -10,18 +10,20 @@ export const ImageUploader = () => {
   return (
     <div className="bg-white flex flex-col space-y-3 rounded-xl p-4 pb-6 mt-4">
       <div className="font-semibold text-sm text-left">가게 사진</div>
-      <ImageUploadButton
-        imageCount={images.length}
-        isFull={isFull}
-        onUpload={addImage}
-      />
-      {isUploading ? (
-        <div className="text-center text-xs text-gray-300">업로드 중...</div>
-      ) : (
-        images.map((image, index) => (
-          <ImagePreview key={index} imgUrl={image} />
-        ))
-      )}
+      <div className="flex flex-row space-x-2">
+        <ImageUploadButton
+          imageCount={images.length}
+          isFull={isFull}
+          onUpload={addImage}
+        />
+        {isUploading ? (
+          <div className="text-center text-xs text-gray-300">업로드 중...</div>
+        ) : (
+          images.map((image, index) => (
+            <ImagePreview key={index} imgUrl={image} />
+          ))
+        )}
+      </div>
     </div>
   );
 };
